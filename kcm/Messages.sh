0f1a7cd33851b1fb6c825acdb123b904eb652f08
Anton Golubev <golubevan@altlinux.org>
2023-01-25 17:07:14 +0300

    Add *.qml to Messages.sh

--- kcm/Messages.sh
+++ kcm/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp"` -o $podir/kcm_lightdm.pot
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp` -o $podir/kcm_lightdm.pot
 rm -f rc.cpp
