fa92c656eeee835a489762b856bddccfe15e1926
Anton Golubev <golubevan@altlinux.org>
2023-01-27 17:23:16 +0300

    Avoid uninitialized struct

--- greeter/keyboard/XcbKeyboardBackend.cpp
+++ greeter/keyboard/XcbKeyboardBackend.cpp
@@ -306,7 +306,7 @@ void XcbKeyboardBackend::dispatchEvents() {
 void XcbKeyboardBackend::connectEventsDispatcher(KeyboardModel *model) {
     // Setup events filter
     xcb_void_cookie_t cookie;
-    xcb_xkb_select_events_details_t foo;
+    xcb_xkb_select_events_details_t foo{};
     xcb_generic_error_t *error = nullptr;
 
     cookie = xcb_xkb_select_events(m_conn, XCB_XKB_ID_USE_CORE_KBD,
